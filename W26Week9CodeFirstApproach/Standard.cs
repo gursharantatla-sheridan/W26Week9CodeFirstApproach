1b6b554fc305bb82b2471a87f3584be679900648
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-03-11 13:42:04 -0400

    student and standard entities

---
+++ W26Week9CodeFirstApproach/Standard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace W26Week9CodeFirstApproach
+{
+    public class Standard
+    {
+        // scalar properties
+        public int StandardId { get; set; }
+        public string? StandardName { get; set; }
+
+        // navigation property
+        public ICollection<Student>? Students { get; set; }
+    }
+}
---
+++ W26Week9CodeFirstApproach/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace W26Week9CodeFirstApproach
+{
+    public class Student
+    {
+        // scalar properties
+        public int StudentId { get; set; }
+        public string? StudentName { get; set; }
+        public int? StandardId { get; set; }
+
+        // navigation property
+        public Standard? Standard { get; set; }
+    }
+}
