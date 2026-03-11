223d698a8146e092c99a2684e63159edbc4c70a2
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-03-11 14:16:41 -0400

    migrations

---
+++ W26Week9CodeFirstApproach/Migrations/20260311180818_InitialMigration.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace W26Week9CodeFirstApproach.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Standards",
+                columns: table => new
+                {
+                    StandardId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StandardName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Standards", x => x.StandardId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StandardId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                    table.ForeignKey(
+                        name: "FK_Students_Standards_StandardId",
+                        column: x => x.StandardId,
+                        principalTable: "Standards",
+                        principalColumn: "StandardId");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Standards",
+                columns: new[] { "StandardId", "StandardName" },
+                values: new object[,]
+                {
+                    { 1, "Standard 1" },
+                    { 2, "Standard 2" },
+                    { 3, "Standard 3" },
+                    { 4, "Standard 4" },
+                    { 5, "Standard 5" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "StandardId", "StudentName" },
+                values: new object[,]
+                {
+                    { 1, 2, "John" },
+                    { 2, 3, "Anne" },
+                    { 3, 1, "Mark" },
+                    { 4, 1, "Sam" },
+                    { 5, 5, "Alice" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_StandardId",
+                table: "Students",
+                column: "StandardId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Standards");
+        }
+    }
+}
