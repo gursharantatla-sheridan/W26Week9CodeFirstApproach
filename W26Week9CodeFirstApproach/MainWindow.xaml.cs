f19ec21a831e7a2b890a872a938855646a6e3f85
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-03-11 15:02:56 -0400

    complete

--- W26Week9CodeFirstApproach/MainWindow.xaml.cs
+++ W26Week9CodeFirstApproach/MainWindow.xaml.cs
@@ -57,5 +57,58 @@ namespace W26Week9CodeFirstApproach
             LoadStudents();
             MessageBox.Show("New student added");
         }
+
+        private void btnFind_Click(object sender, RoutedEventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            var std = db.Students.Find(id);
+
+            if (std != null)
+            {
+                txtName.Text = std.StudentName;
+                cmbStandard.SelectedValue = std.StandardId;
+            }
+            else
+            {
+                txtName.Text = "";
+                cmbStandard.SelectedIndex = -1;
+                MessageBox.Show("Invalid ID. Please try again");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            var std = db.Students.Find(id);
+
+            std.StudentName = txtName.Text;
+            std.StandardId = (int)cmbStandard.SelectedValue;
+
+            db.SaveChanges();
+
+            LoadStudents();
+            MessageBox.Show("Student updated");
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            var std = db.Students.Find(id);
+
+            db.Students.Remove(std);
+            db.SaveChanges();
+
+            LoadStudents();
+            MessageBox.Show("Student deleted");
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            var students = (from s in db.Students
+                            where s.StudentName!.Contains(txtName.Text)
+                            select s).ToList();
+
+            grdStudents.ItemsSource = students;
+        }
     }
 }
